f730c752eb51004f4558c6d6fa18c2794d2b5596
domedome <dome@comenicocatelli.com>
2017-07-10 18:21:54 +0200

    Google Font - Open Sans

--- inc/styles/load-main-styles.php
+++ inc/styles/load-main-styles.php
@@ -8,7 +8,7 @@ function register_styles() {
     wp_enqueue_style( 'italplant-stylesheet', get_stylesheet_uri()  );
 
     // Google Fonts
-    wp_enqueue_style( 'google-font', 'https://fonts.googleapis.com/css?family=Lato:400,600,700|Hind:500,600,700&amp;subset=latin-ext');
+    wp_enqueue_style( 'google-font', 'https://fonts.googleapis.com/css?family=Lato:400,600,700');
 
 }
 add_action('wp_enqueue_scripts', 'register_styles');
